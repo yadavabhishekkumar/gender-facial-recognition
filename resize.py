83fac359120a6e658f27098e0cd34e72c25e7efb
Abhishek kumar yadav <ay70415@gmail.com>
2017-07-01 22:41:52 +0530

    use this script  to resize the images uniformly

---
+++ resize.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+baseheight = 300
+#in range excludes the upper limit no. but includes the lower limit no.
+for i in range(1,16):       #here you need to enter the no. of image file which in my case was 15  so the upper limit should be 16
+	filename='C:\\Users\\DELL\\codes\\Image\\gender\\1('+str(i)+').jpg'        #here you need to make modifications as per your file path
+	img = Image.open(filename)
+	hpercent = (baseheight / float(img.size[1]))
+	wsize = int((float(img.size[0]) * float(hpercent)))
+	img = img.resize((wsize, baseheight),Image.ANTIALIAS)
+	filename2='C:\\Users\\DELL\\codes\\Image\\gender\\1('+str(i)+').jpg'
+	img.save(filename2)
